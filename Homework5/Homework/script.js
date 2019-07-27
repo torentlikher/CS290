4d1e109639bc26e8d6ec16128052f7de29e11408
torentlikher <torentlikher@gmail.com>
2019-07-27 09:53:10 -0400

    version 2 point 0

--- Homework5/Homework/script.js
+++ Homework5/Homework/script.js
@@ -2,19 +2,25 @@ var express = require('express');
 var app = express();
 var handlebars = require("express-handlebars").create({defaultLayout:'main'});
 var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 2565);
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+
 app.listen(2565);
 
 app.post('/', function(req, res){
+    console.log('post route');
     var qParams = [];
     for (var p in req.body){
       qParams.push({'name':p,'value':req.body[p]})
     }
+    console.log('printing qParams');
     console.log(qParams);
+    console.log('printing req.body');
     console.log(req.body);
     var context = {};
     context.dataList = qParams;
@@ -22,6 +28,7 @@ app.post('/', function(req, res){
 });
 
 app.get('/', function(req, res){
+    console.log('get route');
     var qParams = '';
     for(var p in req.query){
         qParams += 'The name ' + p + ' contains ' + req.query[p] + ', ';
@@ -31,15 +38,15 @@ app.get('/', function(req, res){
     qParams += '.';
     var context = {};
     context.dataList = qParams;
-    res.render('/', context);
+    res.render('get-loopback', context);
 });
 
 app.use(function(req, res){
     res.status(404);
-    res.render('404 Error')
+    res.render('404')
 });
 
 app.use(function(req, res){
     res.status(500);
-    res.render('500 Error')
+    res.render('500')
 });
